bb3733ac73820ec8adcb0a96ce1cce4540f61d2c
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-05-17 23:10:06 -0600

    more

--- HardWay/ex_set18.py
+++ HardWay/ex_set18.py
@@ -33,7 +33,7 @@ def cheese_and_crackers(cheese_int, crackers_int):
     print(f"You have {cheese_int} cheeses!")
     print(f"You have {crackers_int} boxes of crackers!")
     print(f"That is {cheese_int + crackers_int}") #i did this instead of the two print statements about a picnic
-
+'''
 print("Give numbers directly to the function 20, 10")
 cheese_and_crackers(20, 10)
 
@@ -47,3 +47,42 @@ cheese_and_crackers(20+12, 20-10)
 
 print("We can also do a combination")
 cheese_and_crackers(cheese+3, 22-cracker)
+'''
+
+#example 20 Functions and Files
+'''
+    example of how functions and files work together 
+    i didnt make a new txt file for this i just used ones already made 
+'''
+
+from sys import argv
+
+script, input_file = argv
+
+def print_all(f):
+    print(f.read())
+
+def rewind(f):
+    f.seek(0)
+
+def print_a_line(line_count, f):
+    print(line_count, f.readline())
+
+current_file = open(input_file)
+
+print("First print the whole file:\n")
+print_all(current_file)
+
+print("Now let's rewind and go back to the start.")
+rewind(current_file)
+
+print("Let's print three lines:")
+
+curr_line = 1 
+print_a_line(curr_line, current_file)
+curr_line += 1
+print_a_line(curr_line, current_file)
+curr_line += 1
+print_a_line(curr_line, current_file)
+
+current_file.close()
