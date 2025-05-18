036f7da00d4918fb951d3e6b399db6a262c4a144
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-05-17 22:27:26 -0600

    more examples

---
+++ HardWay/ex_set18.py
@@ -0,0 +1,49 @@
+#example 18 Names, Variables, Code, Functions
+'''
+finally getting to functions 
+    functions do three things:
+        name pieces of code the way variables name strings and number
+'''
+
+#def declares the function
+def print_two(*args):
+    arg1, arg2 = args #i didnt know you could do *argument for multi
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+def print_two_again(arg1, arg2): #just doing it again 
+    print(f"arg1: {arg1}, arg2: {arg2}")
+
+#then there is one just taking one arg
+#then the one showing dont need to pass arg
+def print_none(): #just doing it again 
+    print("Nothing passed!")
+
+#now call the functions
+#print_two("first", "second")
+#print_two_again("first", "second")
+#print_none()
+
+#example 19 Functions and Variables 
+'''
+    an exercise on how the vars in a funct are not connected to ones outside of it 
+    local vs global stuff
+'''
+
+def cheese_and_crackers(cheese_int, crackers_int):
+    print(f"You have {cheese_int} cheeses!")
+    print(f"You have {crackers_int} boxes of crackers!")
+    print(f"That is {cheese_int + crackers_int}") #i did this instead of the two print statements about a picnic
+
+print("Give numbers directly to the function 20, 10")
+cheese_and_crackers(20, 10)
+
+print("Or we can give values to a variable and pass the variable to the function")
+cheese = 15
+cracker = 11
+cheese_and_crackers(cheese, cracker)
+
+print("We can do math inside too")
+cheese_and_crackers(20+12, 20-10)
+
+print("We can also do a combination")
+cheese_and_crackers(cheese+3, 22-cracker)
