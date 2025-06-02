1a7c3dc556ec21740de0f95b50197af2d384ba50
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-06-01 22:11:05 -0600

    new book

---
+++ BeginToCodeWPython/ch2.py
@@ -0,0 +1,20 @@
+'''Just little examples in the book'''
+print("CH 2")
+print(ord("W")) #ordinal num
+print(ord("w"))
+print(chr(87)) #char rep of ord num
+print(chr(53))
+print(bin(87)) #binary representation
+
+print("CH 3")
+print(2+2)
+
+import random
+print("A Random number from [1,6]")
+print(random.randint(1,6))
+
+import time
+#you can pause your program for a given time 
+print("I need to think")
+time.sleep(5) #sleep for 5 seconds
+print("I have thought")
