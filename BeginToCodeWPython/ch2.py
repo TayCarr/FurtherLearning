cf1f693cbbf1d41181cfb735ef16a04412cfd9b4
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-06-02 12:42:24 -0600

    added stuff
    included snap.py from book auth

--- BeginToCodeWPython/ch2.py
+++ BeginToCodeWPython/ch2.py
@@ -1,20 +1,40 @@
 '''Just little examples in the book'''
-print("CH 2")
-print(ord("W")) #ordinal num
-print(ord("w"))
-print(chr(87)) #char rep of ord num
-print(chr(53))
-print(bin(87)) #binary representation
+def ch2():
+    print("CH 2")
+    print(ord("W")) #ordinal num
+    print(ord("w"))
+    print(chr(87)) #char rep of ord num
+    print(chr(53))
+    print(bin(87)) #binary representation
 
-print("CH 3")
-print(2+2)
+def ch3():
+    print("CH 3")
+    print(2+2)
 
-import random
-print("A Random number from [1,6]")
-print(random.randint(1,6))
+    import random
+    print("A Random number from [1,6]")
+    print(random.randint(1,6))
 
+    import time
+    #you can pause your program for a given time 
+    print("I need to think")
+    time.sleep(5) #sleep for 5 seconds
+    print("I have thought")
+
+import snaps  #snaps is file from author to use with book examples
 import time
-#you can pause your program for a given time 
-print("I need to think")
-time.sleep(5) #sleep for 5 seconds
-print("I have thought")
+def ch3_display():
+    #snaps.display_message("hello world")
+    snaps.display_message("this is a smaller text in green on the top left", color=(0,255,0), size=50, horiz='left', vert='top')
+
+    time.sleep(5) #needed to add this or it would just flash on screen wouldn't get a chance to see it 
+
+def ch3_05():
+    snaps.display_image("capstone.png")
+    snaps.display_message("my capstone", color=(2,2,2), vert="top")
+    snaps.play_sound("ding.wav")
+    time.sleep(3)
+    
+
+#ch3_display()
+ch3_05()
