d67135330f33ee6c50fcf3a37775c98f73484bfe
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-11-13 15:01:06 -0700

    numbers

---
+++ JSAbsoluteBeg/numberExample.js
@@ -0,0 +1,35 @@
+//skipping most stuff tbh
+
+//order matters 
+var i = 4;
+var j = i++ ; //j=4 and i=5
+
+//but if you do it the other way 
+var t = 4;
+var x = ++i; //x and t are 5
+
+//special values
+var bigNum = Infinity;
+var smallNum = -Infinity;
+
+//NaN value "Not a Number"
+//when you do an operation that is invalid NaN gets returned
+var oops = Math.sqrt(-1); //results in NaN
+
+//Math Object
+//Math.something to perform an operation
+//constants 
+//eulers, pi, log
+
+//rounding numbers
+//floor, ceiling, round
+
+//trig functions
+
+//powers sqrts
+
+//absolute values
+
+//random numbers 
+
+//ya didnt really wanna write out the exmples of this it was just using the method w/e 
