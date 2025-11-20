f014fd20b02bb1bdd9906ae86d0d604383636e63
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-11-19 20:05:52 -0700

    setting up new book

---
+++ CProgAbsBeg/ch2ex1.c
@@ -0,0 +1,9 @@
+/*Prints a message on the screen*/
+
+#include <stdio.h>
+
+int main(){ /*compiler yelled at me to declare int for type*/
+    printf("Just one small step for... ya i don't wanna type that.... so this is my typing...");
+    printf(" and my new line\n");
+    return 0;
+}
