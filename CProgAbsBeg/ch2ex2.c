ccef7c11a5badf5a62adff245b133bcd13b4b059
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-11-19 20:32:57 -0700

    next example

---
+++ CProgAbsBeg/ch2ex2.c
@@ -0,0 +1,11 @@
+/*Program that uses characters, ints, float data types*/
+#include <stdio.h>
+
+int main(){
+    printf("I am learning the %c prog language\n", 'C');
+    printf("I have just finished ch %d\n", 2);
+    printf("I am %.lf percent ready to move on ", 99.9);
+    printf("to the next chapter\n");
+    return 0;
+
+}
