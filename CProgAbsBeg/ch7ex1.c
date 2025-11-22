c070bc335d7eecf0f25674c9e8f4ac32670bff78
TayCarr <117793653+TayCarr@users.noreply.github.com>
2025-11-22 09:58:37 -0700

    header files

---
+++ CProgAbsBeg/ch7ex1.c
@@ -0,0 +1,26 @@
+/*Sample program that lists 3 kids and their school supplies and cost*/
+
+#include <stdio.h>
+#include <string.h>
+#include "ch7ex1.h"
+
+int main(){
+    int age;
+    char childname[14] = "Thomas";
+
+    printf("\n%s have %d kids.\n", FAMILY, KIDS);
+
+    age = 11;
+    printf("The oldest, %s, is %d.\n", childname, age);
+
+    stpcpy(childname, "Christopher");
+    age = 6;
+    printf("The middle boy, %s, is %d.\n", childname, age);
+
+    age = 3;
+    strcpy(childname, "Benjamin");
+    printf("The youngest, %s, is %d.\n", childname, age);
+
+    return 0;
+
+}
---
+++ CProgAbsBeg/ch7ex1.h
@@ -0,0 +1,7 @@
+//Example header program for ch7 ex1
+
+//if you have certain values that will not change, or rarely then you can define them in a header file (or in code)
+
+#define KIDS 3
+#define FAMILY "The Peytons"
+#define MORTGAGE_RATE 5.15
