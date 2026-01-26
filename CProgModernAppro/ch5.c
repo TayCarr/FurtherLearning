44bbb76c1571df9fe53a7a4b698eba29cad6a3db
TayCarr <117793653+TayCarr@users.noreply.github.com>
2026-01-26 16:14:50 -0700

    ch5 proj

--- CProgModernAppro/ch5.c
+++ CProgModernAppro/ch5.c
@@ -44,7 +44,8 @@ int main(){
     //ex1();
     //ex2();
     //ex7();
-    ex11();
+    //ex11();
+    ex3();
 
     //ex4();
     //ex5();
@@ -214,9 +215,49 @@ float ex4(void){
 }
 
 //*********************
-//
+/*modify broker.c to ask the user to enter the number of shares and price per share, instead of value of trade 
+ *  AND add statements that compute the commision charged by a rival broker ($33 plus 3 cents per share for 
+ *  fewer than 2000 shares; $33 plus 2 cents per share for 2000 shares or more) display rivals commission as well as original broker*/
 float ex3(void){
-    
+
+    float commission, value, shares, priceps, rival;
+
+    printf("Enter the number of shares: ");
+    scanf(" %f", &shares);
+    printf("Enter the price per share: ");
+    scanf(" %f", &priceps);
+
+    //UMMMM googled calculation commission based on price per share and it said Totalvalue = pps * numShares sooooo...
+
+    value = priceps * shares;
+
+    if(value < 2500.00f)
+        commission = 30.00f + .017f * value;
+    else if( value < 6250.00f)
+        commission = 56.00f + .0066f * value;
+    else if( value < 20000.00f)
+        commission = 76.00f + .0034f * value;
+    else if( value < 50000.00f)
+        commission = 100.00f + .0022f * value;
+    else if( value < 500000.00f)
+        commission = 155.00f + .0011f * value;
+    else
+        commission = 255.00f + .0009f * value;
+
+    if(commission < 39.00f)
+        commission = 39.00f;
+
+    printf("Commission: $%.2f\n", commission);
+
+
+    //i think doing the calcs right idk how to do these commission things lol
+    if(shares < 2000){
+        rival = 33.00f + (.03 * shares);
+    }
+    else
+        rival = 33.00f + (.02 * shares);
+
+    printf("Rivals Commission: $%.2f\n", rival);
 
     return 0.0;
 }
